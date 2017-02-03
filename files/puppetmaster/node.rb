a28d68acf61a4eecb7f3e51e076849c24facde33
sideeffect42 <sideeffect42@users.noreply.github.com>
2017-02-03 10:02:37 +0100

    Specifying ruby path in node.rb

--- files/puppetmaster/node.rb
+++ files/puppetmaster/node.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/opt/puppetlabs/puppet/bin/ruby
 
 # Script usually acts as an ENC for a single host, with the certname supplied as argument
 #   if 'facts' is true, the YAML facts for the host are uploaded
