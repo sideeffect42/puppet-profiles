84cdea29b0a7c00a05e8f07ee21a7dc995728c55
sideeffect42 <sideeffect42@users.noreply.github.com>
2017-02-03 09:57:23 +0100

    Added Foreman integration for puppetmaster

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,15 +1,20 @@
-class profiles::puppetmaster {
+class profiles::puppetmaster (
+  $autosign_domain = true,
+  $foreman_integration = false
+) {
 
   service { 'puppetserver':
     ensure => running,
     enable => true,
   }
 
-  file { "${settings::confdir}/autosign.conf":
-    ensure  => file,
-    content => "*.$domain",
+  file_line { 'autosign.conf':
+    ensure  => $autosign_domain ? { true => present, default => absent },
+    path    => "${settings::confdir}/autosign.conf",
+    line    => "*.$domain",
     notify  => Service['puppetserver'],
   }
+
   file { '/opt/puppetlabs/bin/r10k':
     ensure => 'link',
     target => '/opt/puppetlabs/puppet/bin/r10k'
@@ -21,5 +26,26 @@ class profiles::puppetmaster {
   file { '/etc/puppetlabs/puppetserver/conf.d/product.conf':
     content => template("$module_name/puppetmaster/puppetserver_product_conf.erb"),
     notify => Service['puppetserver'],
+  }
+
+  if $foreman_integration {
+    file { '/etc/puppetlabs/puppet/foreman.yaml':
+      ensure  => file,
+      content => template("$module_name/puppetmaster/foreman.yaml.erb"),
+    }
+
+    file { '/etc/puppetlabs/puppet/node.rb':
+      ensure  => file,
+      source  => "puppet:///modules/$module_name/puppetmaster/node.rb",
+      mode    => '+x',
+      notify  => Service['puppetserver'],
+    }
+  } else {
+    file { '/etc/puppetlabs/puppet/foreman.yaml':
+      ensure => absent,
+    }
+    file { '/etc/puppetlabs/puppet/node.rb':
+      ensure => absent,
+    }
   }
 }
