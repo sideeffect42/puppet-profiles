afffd10c5161b23ac00de26b392107833eb26376
sideeffect42 <sideeffect42@users.noreply.github.com>
2017-02-09 16:38:11 +0100

    RabbitMQ: Setting group of default private key files

--- manifests/rabbitmq.pp
+++ manifests/rabbitmq.pp
@@ -2,4 +2,7 @@ class profiles::rabbitmq {
 
   include ::rabbitmq
 
+  file { ["${settings::ssldir}/private_keys", "${settings::ssldir}/private_keys/${::fqdn}.pem"]:
+    group => 'rabbitmq',
+  }
 }
