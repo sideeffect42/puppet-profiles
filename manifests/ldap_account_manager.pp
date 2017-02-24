fc9ebc06a214102e9b43107f080b3861e9af3c22
sideeffect42 <sideeffect42@users.noreply.github.com>
2017-02-24 16:09:03 +0100

    Improved ldap_account_manager profile

--- manifests/ldap_account_manager.pp
+++ manifests/ldap_account_manager.pp
@@ -1,7 +1,39 @@
 class profiles::ldap_account_manager {
+  # configure Apache httpd web server
+  class { '::apache':
+    default_vhost => false,
+    mpm_module    => 'prefork',
+  }
+  include ::apache::mod::php
+  include ::apache::mod::rewrite
+
+  apache::vhost { 'ldap-account-manager':
+    port => 80,
+    docroot => '/usr/share/ldap-account-manager',
+  }
+  apache::vhost { 'ssl-ldap-account-manager':
+    port => 443,
+    docroot => '/usr/share/ldap-account-manager',
+  }
+
+  $php_prefix = 'php' # TODO: set correctly based on system version
   ensure_packages({
     'ldap-account-manager' => {
       ensure => present,
+      notify => Service['apache2'],
     },
+    "$php_prefix-xml" => {
+      ensure => present,
+      notify => Service['apache2'],
+    },
+    "$php_prefix-zip" => {
+       ensure => present,
+       notify => Service['apache2'],
+    }
   })
+
+  file { '/etc/apache2/conf.d/ldap-account-manager':
+    ensure => link,
+    target => '/etc/ldap-account-manager/apache.conf',
+  }
 }
