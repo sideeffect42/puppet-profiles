06bf5fd94f093f759677b01535c11d3385a4578f
sideeffect42 <sideeffect42@users.noreply.github.com>
2017-03-03 11:31:38 +0100

    LAM: Made HTTP and HTTPS services configurable

--- manifests/ldap_account_manager.pp
+++ manifests/ldap_account_manager.pp
@@ -1,5 +1,8 @@
 class profiles::ldap_account_manager(
   $vhost_domain = undef,
+  $http = true,
+  $https = false,
+  $hsts = true,
 ) {
   # configure Apache httpd web server
   class { '::apache':
@@ -9,16 +12,33 @@ class profiles::ldap_account_manager(
   include ::apache::mod::php
   include ::apache::mod::rewrite
 
-  apache::vhost { 'ldap-account-manager':
-    port       => 80,
-    docroot    => '/usr/share/ldap-account-manager',
-    servername => "$vhost_domain",
+  if $http {
+    apache::vhost { 'ldap-account-manager':
+      port       => 80,
+      docroot    => '/usr/share/ldap-account-manager',
+      servername => "$vhost_domain",
+    }
   }
-  apache::vhost { 'ssl-ldap-account-manager':
-    ssl        => true,
-    port       => 443,
-    docroot    => '/usr/share/ldap-account-manager',
-    servername => "$vhost_domain",
+  elsif $https {
+    apache::vhost { 'ldap-account-manager':
+      port            => 80,
+      redirect_status => 'permanent',
+      redirect_dest   => $vhost_domain ? {
+        undef   => "https://$::fqdn/",
+        default => "https://$vhost_domain/"
+      },
+    }
+  }
+  if $https {
+    apache::vhost { 'ssl-ldap-account-manager':
+      ssl        => true,
+      port       => 443,
+      docroot    => '/usr/share/ldap-account-manager',
+      servername => "$vhost_domain",
+      headers    => [
+        $hsts ? { true => 'set Strict-Transport-Security: max-age=31536000', default => undef }
+      ],
+    }
   }
 
   $php_prefix = 'php' # TODO: set correctly based on system version
